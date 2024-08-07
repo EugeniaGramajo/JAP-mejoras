f55e54a8a386fc7d9d822c61edddc56ac4a01190
Bryan <bryanribero@hotmail.com>
2024-08-07 20:57:41 -0300

    Agregue la funcion para que haga el cambio de la imagen

---
+++ programacion.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+   
+    let imagen = document.getElementsByClassName('image')[0]; 
+    let boton = document.getElementById('cambio');
+
+   
+    let imagen1 = 'img1.jpg';
+    let imagen2 = 'img2.jpg';
+
+    
+    let imagenActual = imagen1;
+
+    boton.addEventListener('click', function() {
+        if (imagenActual === imagen1) {
+            imagen.src = imagen2;
+            imagenActual = imagen2;
+        } else {
+            imagen.src = imagen1;
+            imagenActual = imagen1;
+        }
+    });
+});
